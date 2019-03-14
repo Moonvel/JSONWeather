dc198a0db8e1920490d1e9bbe339031665609f98
Константин Мельниченко <moonvel@yandex.ru>
2019-03-14 19:12:59 +0300

    Добавьте файлы проекта.

---
+++ JSONWeather/GET_JSONdata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace JSONWeather
+{
+    public class GET_JSONdata
+    {
+        
+       public static JSONClass Data(string API, string City)
+        {
+            using (var webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(string.Format("http://api.openweathermap.org/data/2.5/weather?appid={0}&q={1}", API, City));
+
+                JSONClass jSON = JsonConvert.DeserializeObject<JSONClass>(json);
+                
+
+                return jSON;
+
+                
+
+
+
+            }
+        }
+
+            
+    }
+}
---
+++ JSONWeather/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONWeather
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var JSONdata = GET_JSONdata.Data("6ff93bf8e8d4c916ca907ae6def4c864", "Bryansk");
+
+            Console.WriteLine();
+        }
+    }
+}
