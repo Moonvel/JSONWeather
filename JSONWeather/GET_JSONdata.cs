6cc12f68ee0b44a88fe63fe9be10ae86c5a5aebf
Константин Мельниченко <moonvel@yandex.ru>
2019-03-15 19:11:21 +0300

    Contract & Serialization

--- JSONWeather/GET_JSONdata.cs
+++ JSONWeather/GET_JSONdata.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace JSONWeather
 {
@@ -13,22 +14,36 @@ namespace JSONWeather
         
        public static RootObject Data(string API, string City)
         {
-            using (var webClient = new WebClient())
+         
+            WebRequest request = WebRequest.Create(string.Format("http://api.openweathermap.org/data/2.5/weather?appid={0}&q={1}", API, City));
+            WebResponse response = request.GetResponse();
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string json = reader.ReadToEnd();
+
+                    RootObject JSON = JsonConvert.DeserializeObject<RootObject>(json);
+
+                    return JSON;
+                }
+            }
+
+
+
+            /*  using (var webClient = new WebClient())
             {
                 var json = webClient.DownloadString(string.Format("http://api.openweathermap.org/data/2.5/weather?appid={0}&q={1}", API, City));
 
-                RootObject jSON = JsonConvert.DeserializeObject<RootObject>(json);
+                RootObject JSON = JsonConvert.DeserializeObject<RootObject>(json);
 
                 
-                return jSON;
+                return JSON; 
 
-                
-
-
-
-            }
+            } */
         }
 
-            
+
     }
 }
