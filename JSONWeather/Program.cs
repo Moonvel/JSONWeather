3be00bb5cc8f071ce2a6ba17fbd8d47ffb619826
Константин Мельниченко <moonvel@yandex.ru>
2019-03-15 19:30:47 +0300

    Revert "Revert "Contract & Serialization""
    This reverts commit e3f9b1d989baa5a57b9ad1feca3e3b7728288be5.

--- JSONWeather/Program.cs
+++ JSONWeather/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace JSONWeather
 {
     public class Program
@@ -16,8 +18,12 @@ namespace JSONWeather
             city = Console.ReadLine();
             var JSONdata = GET_JSONdata.Data("6ff93bf8e8d4c916ca907ae6def4c864", city);
 
+            Console.WriteLine("Тест: ");
             Console.WriteLine($"{JSONdata.Weather[0].Main} + {JSONdata.Visibility}");
-               
+
+            Console.WriteLine("JSON в *.txt: ");
+            Serialization.CreateJSONFile(JSONdata);
+
 
 
 
